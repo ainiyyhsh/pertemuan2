76220b0eee94539a04325b1b039beb4c8c031514
ainiyyhsh <ainiyyahsihirwn@gmail.com>
2021-09-25 21:22:07 +0700

    https://github.com/ainiyyhsh/pertemuan2.git

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ainiyyahsih Irawan UBSI Jatiwaringin dengan NIM 19200645";
+}
